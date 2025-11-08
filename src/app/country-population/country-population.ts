e2a4fce1d31c57a6101f7235fc08b17de6ccd4ed
jjforsyth15 <jjforsyth15@gmail.com>
2025-11-07 17:12:18 -0800

    Updates from missed class 10/24/2025

--- src/app/country-population/country-population.ts
+++ src/app/country-population/country-population.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PopulationData } from './population-data';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment.development';
-
+import { ActivatedRoute, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-country-population',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './country-population.html',
   styleUrl: './country-population.scss'
 })
-export class CountryPopulation {
-  country!: PopulationData;
-  constructor(http: HttpClient) {
-    http.get<PopulationData>(environment.apiUrl + "/api/Countries/population").subscribe(result => {
-      this.country = result;
-    });
+export class CountryPopulation implements OnInit {
+  population!: PopulationData;
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {
+
+   }
+   ngOnInit(): void {
+     let idparam = this.activatedRoute.snapshot.paramMap.get('id');
+     this.http.get<PopulationData>
+     (`${environment.apiUrl}/api/Countries/population/${idparam}`).subscribe(result => {
+       this.population = result;
+     });
+   }
   } 
-}
